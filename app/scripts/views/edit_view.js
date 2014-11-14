f7ff0bd9b4f57d9be6cddc659339b4b29e8a5435
Thomas McCracken <thomas.alan.mccracken@gmail.com>
2014-11-14 13:59:12 -0500

    corrected edit view

--- app/scripts/views/edit_view.js
+++ app/scripts/views/edit_view.js
@@ -13,6 +13,7 @@
       this.options = options;
       console.log(this.options.post.toJSON());
       this.render();
+      console.log(this.options.post.toJSON());
 
       $('#viewContainer').html(this.$el);
     },
@@ -27,7 +28,7 @@
       this.options.post.set({
         title: $('#editPostTitle').val(),
         content: $('#editPostContent').val(),
-        category:[],
+        category:[]
       });
 
       console.log('edit' + this);
