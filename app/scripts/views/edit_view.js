25db85e3296cf157aeb7b22660f0d0e0a39d804d
Matt Griffeth <mgriffeth9@gmail.com>
2014-11-14 13:40:02 -0500

    stuff

--- app/scripts/views/edit_view.js
+++ app/scripts/views/edit_view.js
@@ -2,6 +2,11 @@
 
   App.Views.EditPost = Parse.View.extend({
 
+    events:{
+      'click #editPostSubmit': 'submitEdit',
+      'click #deletePost':'deletePost'
+    },
+
     template: _.template($('#editPostTemp').html()),
 
     initialize: function (options) {
@@ -14,6 +19,30 @@
 
     render: function () {
       this.$el.html(this.template(this.options.post.toJSON()));
+    },
+
+    submitEdit:function(e){
+      e.preventDefault();
+
+      this.options.post.set({
+        title: $('#editPostTitle').val(),
+        content: $('#editPostContent').val(),
+        category:[],
+      });
+
+      console.log('edit' + this);
+      this.options.post.save();
+      App.router.navigate('',{ trigger : true });
+    },
+
+    deletePost:function(e){
+      e.preventDefault();
+      console.log('delete')
+
+      this.options.post.destroy();
+      App.router.navigate('',{ trigger : true });
+
+
     }
   });
 }());
