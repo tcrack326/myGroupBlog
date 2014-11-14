172a33bbd5608951db2856ab326056774dc3433e
Thomas McCracken <thomas.alan.mccracken@gmail.com>
2014-11-14 14:32:23 -0500

    finished authorization for user

--- app/scripts/views/single_view.js
+++ app/scripts/views/single_view.js
@@ -8,11 +8,26 @@
       this.options = options;
       this.render();
       $('#viewContainer').html(this.$el);
+
+      //Hide the edit button if the author is different than the user
+      App.user = Parse.User.current();
+      if (App.user === null){
+        $('#editBtn').hide();
+      }
+
+      else if (App.user.id != this.options.post.attributes.author.id){
+         $('#editBtn').hide();
+      }
     },
 
     render: function () {
       // var self = this;
       //this.$el.html(this.template);
+
+
+      //if(this.options.post.id != App.user.id){
+      //  console.log("hey there");
+    //  }
       this.$el.html(this.template(this.options.post.toJSON()));
 
       // var postQuery = new Parse.Query(App.Models .PostModel);
