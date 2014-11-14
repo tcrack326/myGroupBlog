beeee8adb661ed3ee4bf8345e3a999aa6f87ff98
Thomas McCracken <thomas.alan.mccracken@gmail.com>
2014-11-14 14:38:14 -0500

    fixed conflict

--- app/scripts/views/edit_view.js
+++ app/scripts/views/edit_view.js
@@ -28,11 +28,7 @@
       this.options.post.set({
         title: $('#editPostTitle').val(),
         content: $('#editPostContent').val(),
-<<<<<<< HEAD
-        
-=======
         category:[]
->>>>>>> f7ff0bd9b4f57d9be6cddc659339b4b29e8a5435
       });
 
       console.log('edit' + this);
