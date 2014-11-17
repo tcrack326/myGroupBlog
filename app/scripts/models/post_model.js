8da543dd9ecf2ee711f72c32fd9675925cb22981
Matt Griffeth <mgriffeth9@gmail.com>
2014-11-16 23:58:35 -0500

    ok

--- app/scripts/models/post_model.js
+++ app/scripts/models/post_model.js
@@ -8,12 +8,9 @@
     defaults:{
       title:'',
       content:'',
-<<<<<<< HEAD
-      category:'',
-=======
       category:[],
       published:false
->>>>>>> b3c8762c581599ac0cddca8be16e0607b8a1bb0e
+
     },
 
 
