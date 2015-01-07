47ac3c95859651b3be81bbe20d875d6cfce5a793
Thomas McCracken <thomas.alan.mccracken@gmail.com>
2015-01-07 15:12:38 -0500

    fixed and gulped

--- scripts/main.js
+++ scripts/main.js
@@ -1,76 +1,3 @@
-Parse.initialize("gLBx6znzsrsBwyvjWsdrxKkrKn7MIQHlA2BTkhMR", "bh8PUYEckGb4ZeWjNQ8tv9POmTvzzN3L6NDWnzSq");
-
-(function(){
-
-  App.user = Parse.User.current();
-
-  App.all_posts = new App.Collections.PostCollection();
-  App.all_comments = new App.Collections.CommentCollection();
-  App.all_comments.fetch();
-
-  App.all_posts.fetch().done(function(){
-
-    App.router = new App.Routers.BlogRouter();
-
-    Parse.history.start();
-
-
-
-  });
-
-  $('#logoutBtn').on('click', function (e) {
-      e.preventDefault();
-      e.stopPropagation();
-      Parse.User.logOut();
-      $('#addPostBtn').text('Signup');
-      $('#addPostBtn').attr('id', 'signUpBtn');
-      $('#logoutBtn').text('Login');
-      $('#logoutBtn').attr('id', 'loginBtn');
-      App.updateUser();
-      App.router.navigate('', { trigger: true });
-  });
-
-  $('#loginBtn').on('click', function (e) {
-      e.preventDefault();
-      e.stopPropagation();
-      App.router.navigate('login', { trigger: true });
-      App.updateUser();
-  });
-
-  $('#signUpBtn').on('click', function (e) {
-      e.preventDefault();
-      e.stopPropagation();
-      App.router.navigate('signup', { trigger: true });
-      App.updateUser();
-  });
-  //
-  // $('#addPostBtn').on('click', function () {
-  //     e.preventDefault();
-  //     e.stopPropagation();
-  //     App.router.navigate('newpost', { trigger: true });
-  // });
-
-
-  App.updateUser = function () {
-    App.user = Parse.User.current();
-    var currentUser;
-    if (App.user === null){
-      currentUser = '';
-      $('#signature').text('');
-    }
-    else {
-      currentUser = App.user.attributes.username;
-      $('#signature').text('Welcome, ' + currentUser);
-      $('#signUpBtn').text('Add New Post');
-      $('#signUpBtn').attr('id', 'addPostBtn');
-      $('#loginBtn').text('Logout');
-      $('#loginBtn').attr('id', 'logoutBtn');
-
-    }
-  }
-
-}());
-
 (function(){
   App.Views.Login = Parse.View.extend({
 
@@ -888,4 +815,77 @@ App.Views.CategoryView = Parse.View.extend({
   App.Collections.CommentCollection = Parse.Collection.extend({
     model: App.Models.CommentModel
   });
+}());
+
+Parse.initialize("gLBx6znzsrsBwyvjWsdrxKkrKn7MIQHlA2BTkhMR", "bh8PUYEckGb4ZeWjNQ8tv9POmTvzzN3L6NDWnzSq");
+
+(function(){
+
+  App.user = Parse.User.current();
+
+  App.all_posts = new App.Collections.PostCollection();
+  App.all_comments = new App.Collections.CommentCollection();
+  App.all_comments.fetch();
+
+  App.all_posts.fetch().done(function(){
+
+    App.router = new App.Routers.BlogRouter();
+
+    Parse.history.start();
+
+
+
+  });
+
+  $('#logoutBtn').on('click', function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      Parse.User.logOut();
+      $('#addPostBtn').text('Signup');
+      $('#addPostBtn').attr('id', 'signUpBtn');
+      $('#logoutBtn').text('Login');
+      $('#logoutBtn').attr('id', 'loginBtn');
+      App.updateUser();
+      App.router.navigate('', { trigger: true });
+  });
+
+  $('#loginBtn').on('click', function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      App.router.navigate('login', { trigger: true });
+      App.updateUser();
+  });
+
+  $('#signUpBtn').on('click', function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      App.router.navigate('signup', { trigger: true });
+      App.updateUser();
+  });
+  //
+  // $('#addPostBtn').on('click', function () {
+  //     e.preventDefault();
+  //     e.stopPropagation();
+  //     App.router.navigate('newpost', { trigger: true });
+  // });
+
+
+  App.updateUser = function () {
+    App.user = Parse.User.current();
+    var currentUser;
+    if (App.user === null){
+      currentUser = '';
+      $('#signature').text('');
+    }
+    else {
+      currentUser = App.user.attributes.username;
+      $('#signature').text('Welcome, ' + currentUser);
+      $('#signUpBtn').text('Add New Post');
+      $('#signUpBtn').attr('id', 'addPostBtn');
+      $('#loginBtn').text('Logout');
+      $('#loginBtn').attr('id', 'logoutBtn');
+
+    }
+  }
+
 }());
